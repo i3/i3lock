603c90e9859f30671841bbc305e53fbb7bf7fee8
Michael Stapelberg <michael@stapelberg.de>
2012-09-26 11:21:48 +0200

    promote the "could not load image" message from debug to normal

--- i3lock.c
+++ i3lock.c
@@ -692,9 +692,8 @@ int main(int argc, char *argv[]) {
         img = cairo_image_surface_create_from_png(image_path);
         /* In case loading failed, we just pretend no -i was specified. */
         if (cairo_surface_status(img) != CAIRO_STATUS_SUCCESS) {
-            if (debug_mode)
-                fprintf(stderr, "Could not load image \"%s\": cairo surface status %d\n",
-                        image_path, cairo_surface_status(img));
+            fprintf(stderr, "Could not load image \"%s\": cairo surface status %d\n",
+                    image_path, cairo_surface_status(img));
             img = NULL;
         }
     }
