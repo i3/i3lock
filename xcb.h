2e4e5d2e9b295f7bfb2d17d9b34ab5f3189e6bb6
Leandro Vital <leandro.vital@yahoo.com.br>
2016-09-11 20:53:43 -0300

    includes a flag -g for blurring a screenshot of your desktop, used alone this will attempt to capture the entire screen contents and blur it. however, this may also be used in conjunction with both a loaded image file and the tiling option

--- xcb.h
+++ xcb.h
@@ -9,6 +9,7 @@ extern xcb_screen_t *screen;
 
 xcb_visualtype_t *get_root_visual_type(xcb_screen_t *s);
 xcb_pixmap_t create_bg_pixmap(xcb_connection_t *conn, xcb_screen_t *scr, u_int32_t *resolution, char *color);
+xcb_pixmap_t capture_bg_pixmap(xcb_connection_t *conn, xcb_screen_t *scr, u_int32_t* resolution);
 xcb_window_t open_fullscreen_window(xcb_connection_t *conn, xcb_screen_t *scr, char *color, xcb_pixmap_t pixmap);
 void grab_pointer_and_keyboard(xcb_connection_t *conn, xcb_screen_t *screen, xcb_cursor_t cursor);
 void dpms_set_mode(xcb_connection_t *conn, xcb_dpms_dpms_mode_t mode);
