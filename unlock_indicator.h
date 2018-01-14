ac21791f4e65276c2325bae8739e790073853101
Michael Stapelberg <michael@stapelberg.de>
2018-01-14 21:20:26 +0100

    display error when backspace is pressed without any input
    This adds some feedback to a keypress which previously had no visible effect.
    
    While the text “no input” isn’t the most descriptive, it was the only one I
    could think of which fit into the unlock indicator circle. If you have a better
    suggestion, let me know.
    
    fixes #164

--- unlock_indicator.h
+++ unlock_indicator.h
@@ -2,20 +2,21 @@
 #define _UNLOCK_INDICATOR_H
 
 typedef enum {
-    STATE_STARTED = 0,         /* default state */
-    STATE_KEY_PRESSED = 1,     /* key was pressed, show unlock indicator */
-    STATE_KEY_ACTIVE = 2,      /* a key was pressed recently, highlight part
+    STATE_STARTED = 0,           /* default state */
+    STATE_KEY_PRESSED = 1,       /* key was pressed, show unlock indicator */
+    STATE_KEY_ACTIVE = 2,        /* a key was pressed recently, highlight part
                                    of the unlock indicator. */
-    STATE_BACKSPACE_ACTIVE = 3 /* backspace was pressed recently, highlight
+    STATE_BACKSPACE_ACTIVE = 3,  /* backspace was pressed recently, highlight
                                    part of the unlock indicator in red. */
+    STATE_NOTHING_TO_DELETE = 4, /* backspace was pressed, but there is nothing to delete. */
 } unlock_state_t;
 
 typedef enum {
-    STATE_AUTH_IDLE = 0,         /* no authenticator interaction at the moment */
-    STATE_AUTH_VERIFY = 1,       /* currently verifying the password via authenticator */
-    STATE_AUTH_LOCK = 2,         /* currently locking the screen */
-    STATE_AUTH_WRONG = 3,        /* the password was wrong */
-    STATE_I3LOCK_LOCK_FAILED = 4 /* i3lock failed to load */
+    STATE_AUTH_IDLE = 0,          /* no authenticator interaction at the moment */
+    STATE_AUTH_VERIFY = 1,        /* currently verifying the password via authenticator */
+    STATE_AUTH_LOCK = 2,          /* currently locking the screen */
+    STATE_AUTH_WRONG = 3,         /* the password was wrong */
+    STATE_I3LOCK_LOCK_FAILED = 4, /* i3lock failed to load */
 } auth_state_t;
 
 xcb_pixmap_t draw_image(uint32_t* resolution);
