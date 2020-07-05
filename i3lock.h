6862e4df3ed6d0f0d31b5dbfea2e126a7b1a615a
Michael Stapelberg <michael@stapelberg.de>
2020-07-05 08:48:13 +0200

    make --debug output go to stderr

--- i3lock.h
+++ i3lock.h
@@ -5,10 +5,11 @@
  * This is important because xautolock (for example) closes stdout/stderr by
  * default, so just printing something to stdout will lead to the data ending
  * up on the X11 socket (!). */
-#define DEBUG(fmt, ...)                                   \
-    do {                                                  \
-        if (debug_mode)                                   \
-            printf("[i3lock-debug] " fmt, ##__VA_ARGS__); \
+#define DEBUG(fmt, ...)                                            \
+    do {                                                           \
+        if (debug_mode) {                                          \
+            fprintf(stderr, "[i3lock-debug] " fmt, ##__VA_ARGS__); \
+        }                                                          \
     } while (0)
 
 #endif
